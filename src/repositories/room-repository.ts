f83f72dd6923d3492502d1a44518e8a110f6b8bd
Pedro Nascimento <sn.jaypee@gmail.com>
2023-10-18 16:20:37 -0300

    Add cache to rooms repository

--- src/repositories/room-repository.ts
+++ src/repositories/room-repository.ts
@@ -1,15 +1,35 @@
+import { Room } from '@prisma/client';
 import { prisma } from '@/config';
+import { redis } from '@/config/cache';
 
 async function findAllByHotelId(hotelId: number) {
-  return prisma.room.findMany({
+  const cache = await redis.get('rooms:all');
+
+  if (cache) {
+    const data: Room[] = JSON.parse(cache);
+    return data;
+  }
+
+  const data = await prisma.room.findMany({
     where: { hotelId },
   });
+  await redis.set('rooms:all', JSON.stringify(data));
+  return data;
 }
 
 async function findById(roomId: number) {
-  return prisma.room.findFirst({
+  const cache = await redis.get(`rooms:${roomId}`);
+
+  if (cache) {
+    const data: Room = JSON.parse(cache);
+    return data;
+  }
+
+  const data = await prisma.room.findFirst({
     where: { id: roomId },
   });
+  await redis.set(`rooms:${roomId}`, JSON.stringify(data));
+  return data;
 }
 
 export const roomRepository = {
