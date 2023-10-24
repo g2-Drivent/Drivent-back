3da92e8005612107c7a538be561d722f375f3465
andressafirmino <andressafirmino78@gmail.com>
2023-10-24 16:02:12 -0300

    feat: activitiesRouter

---
+++ src/routers/activities-router.ts
@@ -0,0 +1,14 @@
+import { activitiesController } from "@/controllers";
+import { authenticateToken } from "@/middlewares";
+import { Router } from "express";
+
+const activitiesRouter = Router();
+
+activitiesRouter
+    .all('/*', authenticateToken)
+    .get('/', activitiesController.getAvailableDays)
+    .get('/register', activitiesController.getAvailableTimes)
+    .post('/register', activitiesController.postActivity);
+
+export {activitiesRouter};
+
